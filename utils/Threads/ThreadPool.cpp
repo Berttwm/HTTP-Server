dbdcba9d8bdbf88789fc81809d0ebe6467f56ad8
Berttwm <BertrandTWM@gmail.com>
2021-11-21 22:43:09 +0800

    [Feature] Finish ThreadPool implementation, TODO: Integrate with HTTP server

--- utils/Threads/ThreadPool.cpp
+++ utils/Threads/ThreadPool.cpp
@@ -2,5 +2,59 @@
 
 ThreadPool::ThreadPool()
 {
-    
+    std::cout << "==== Initializing ThreadPool... ====" << std::endl;
+    num_threads = num_threads = std::thread::hardware_concurrency();
+    for(int i = 0; i < num_threads; i++)
+    {
+        thread_pool.push_back(std::thread(&ThreadPool::start_thread));
+    }
+    std::cout << "==== Finished Initializing (number of threads = " << num_threads << ") ThreadPool ====" << std::endl;
+}
+
+void ThreadPool::start_thread()
+{
+    std::function<void()> job;
+    while (true)
+    {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            condition.wait(lock, [this]() {return !queue.empty() || !m_accept_functions; });
+            if (!m_accept_functions && queue.empty())
+            {
+                //lock will be release automatically.
+                //finish the thread loop and let it join in the main thread.
+                return;
+            }
+            job = queue.front();
+            queue.pop();
+            //release the lock
+        }
+        job();
+    }
+}
+
+void ThreadPool::add_job(std::function<void()> New_Job)
+{
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    queue.push(New_Job);
+    // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+    lock.unlock();
+    condition.notify_one();
+}
+
+void ThreadPool::terminate()
+{
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    m_accept_functions = false;
+    lock.unlock();
+    // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+    condition.notify_all();
+    //notify all waiting threads.
+
+    for(std::thread &th : thread_pool)
+    {
+        th.join();
+    }
+    thread_pool.clear();
+    // stopped = true;
 }
--- utils/Threads/ThreadPool.h
+++ utils/Threads/ThreadPool.h
@@ -2,12 +2,27 @@
 #define ThreadPool_H
 
 #include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <iostream>
+#include <condition_variable>
 
 class ThreadPool 
 {
 private:
+    int num_threads;
+    std::vector<std::thread> thread_pool;
+    std::queue<std::function<void()>> queue; // queue for storing jobs
+    std::condition_variable condition;
+    std::mutex queue_mutex;
+
+    bool m_accept_functions = true;
 public:
     ThreadPool();
+    void start_thread();
+    void add_job(std::function<void()> New_Job);
+    void terminate();
 };
 
 #endif // ThreadPool_H
